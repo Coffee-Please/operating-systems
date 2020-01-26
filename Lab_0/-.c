ebeeed50ded92ab0319e5d21f28977c9e58c1577
Coffee-Please <47074173+Coffee-Please@users.noreply.github.com>
2020-01-25 18:17:27 -0800

    Create -.c

---
+++ Lab_0/-.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+//Local Variables
+ 
+//Get user input
+
+//Read and store binary
+
+//Convert to ASCII
+
+//Convert to Decimal
+
+//Format and Print to stdout
+
+return 0;
+}
