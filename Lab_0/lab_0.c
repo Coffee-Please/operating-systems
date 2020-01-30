0ab39ca8bd859cb708bd1ad5ee1a3930dbbd2bde
Cofee-Please <priya_singh@LAPTOP-J1E64D38.localdomain>
2020-01-29 19:40:23 -0800

    testing commit

--- Lab_0/lab_0.c
+++ Lab_0/lab_0.c
@@ -1,8 +1,16 @@
+//Lab 0 for COMP 322/L
+//Created by Priya Singh on 1/29/2020
+
 #include <stdio.h>
+
+
 int main()
 {
-//Get user input
 
+//Get user input
+	printf("Hello World!");
+
+	return 0;
 
 }
 
