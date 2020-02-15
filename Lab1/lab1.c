eabcb0691bea69639ce607b4531dfbe129ba7fcb
Cofee-Please <priya_singh@LAPTOP-J1E64D38.localdomain>
2020-02-15 11:44:16 -0800

    let's try again

---
+++ Lab1/lab1.c
@@ -0,0 +1,29 @@
+//Created 2/15/2020 by Priya Singh
+//COMP 322/L Lab 1: Time and Forking Children
+
+#include <stdio.h>
+
+
+
+int main()
+{
+
+//The program prints the number of seconds since..         (see time(2))
+//The program creates a child process             (see fork(2))
+//The program will wait for the child to finish            (see waitpid(2))
+//The program and its child reports on the information
+//The process ID of its parent              (see getppid(2))
+//Its own process ID                    (see getpid(2))
+//The process ID of its child (if applicable)        (see fork(2))
+//The return status of its child (if applicable)        (see exit(3), waitpid(2))
+//The program will report the following time information    (see times(2))
+//user time
+// system time
+// user time of child
+// system time of child
+//The program prints the number of seconds since..    (see time(2))
+
+
+	printf(":D\n");
+	return 0;
+}//end main
