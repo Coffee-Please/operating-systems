9ef12aef3f3d9026175e00b6b463be39c1365471
Cofee-Please <priya_singh@LAPTOP-J1E64D38.localdomain>
2020-05-16 21:51:26 -0700

    2

--- Lab7/matrix_gen.c
+++ Lab7/matrix_gen.c
@@ -2,13 +2,48 @@
 
 #include <stdio.h>
 
-int main()
-{
-//Open file
+int next;
 
-//Write to file
+//Generates random number
+int rand()
+{
+	next = (((next * 1234022) / 12345) % 37842);
+	return next;
+}//end rand
+
+//Seeds srand
+void srand(int seed)
+{
+	next = seed;
+}//end srand
+
+
+int main(int argc, char** argv)
+{
+//Variables
+	FILE* fp;
+	int num, size_1, size_2;
+
+//Converts args into ints
+
+//Seed srand
+	srand(time(NULL));
+
+//Open file
+	fp = fopen("file.txt", "a");
+
+//For size x size integers
+	for(int i = 0; i < (size_1 * size_2); i++)
+	{
+	//Generate random numbers from -100 to 100
+		num = ((rand() % 200) + 1) - 100;
+
+	//Write to file
+		fputs(fp, num, "a");
+	}//end for
 
 //close file
+	fclose(fp);
 
 	return 0;
 }//end main
